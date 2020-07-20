56812fa26da4f35f38a3675d5f7a1b1eb4e9e684
CharanMareedu-REISys <charan.mareedu@reisystems.com>
2020-07-20 10:57:26 -0400

    new changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'sam-api-challenge'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sam-api-challenge');
+    expect(app.profileForm.get('pscValue')[0]).toEqual('sam-api-challenge');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'sam-api-challenge';
+
+  tiles: Tile[] = [
+    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 3, rows: 1, color: 'lightgreen'}
+  ];
+  options = {
+    pscValue : ['Onefasfadsfafsdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdffadsfasdfasdfa', 'Two', 'Three']
+  };
+  pscFilteredOptions: Observable<string[]>;
+
+  profileForm = this.fb.group({
+    fromDate: [],
+    pscValue: [''],
+    textValue: ['']
+  });
+
+  
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(){
+    this.pscFilteredOptions = this.valueChanges('pscValue');
+  }
+
+  valueChanges(formField){
+    return this.profileForm.get(formField).valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value,formField))
+    );
+  }
+
+  private _filter(value: string,formField: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options[formField].filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
 }
